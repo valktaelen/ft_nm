6b7a83c72704528cfaccc454d6afef1e98a70102
valktaelen <alix.artiges@hotmail.fr>
2023-10-24 06:33:24 +0200

    [FIX] print 64-bits values

--- lib/64/print.c
+++ lib/64/print.c
@@ -6,25 +6,25 @@
 /*   By: aartiges <aartiges@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 06:52:13 by aartiges          #+#    #+#             */
-/*   Updated: 2023/10/23 07:27:18 by aartiges         ###   ########lyon.fr   */
+/*   Updated: 2023/10/24 06:00:15 by aartiges         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib.h"
 
-static void	ft_print_one_64(t_symbol_64 *sym)
+void	ft_print_one_64(t_symbol_64 *sym)
 {
 	if (sym->type != 'U' && sym->type != 'w')
-		print_number_n_digit(sym->st_value, 8);
+		print_number_n_digit(sym->st_value, 16);
 	else if (
 		(sym->info_type == STT_NOTYPE
 			&& sym->bind == STB_LOCAL
 			&& sym->visibility == STV_DEFAULT
 			&& sym->st_shndx == SHN_UNDEF)
 		|| sym->st_shndx == SHN_ABS || sym->info_type == STT_SECTION)
-		print_number_n_digit(0, 8);
+		print_number_n_digit(0, 16);
 	else
-		write(1, "        ", 8);
+		write(1, "                ", 16);
 	write(1, SEP_SPACE, 1);
 	write(1, &sym->type, 1);
 	write(1, SEP_SPACE, 1);

b17bb61ba4684a8578da5ff8d9c1668c27b869c3
valktaelen <alix.artiges@hotmail.fr>
2023-10-25 07:46:47 +0200

    [FIX] change symbol print

--- lib/32/print.c
+++ lib/32/print.c
@@ -14,7 +14,7 @@
 
 static void	ft_print_one_32(t_symbol_32 *sym)
 {
-	if (sym->type != 'U' && sym->type != 'w')
+	if (sym->type != 'U' && sym->type != 'w' && sym->type != 'v')
 		print_number_n_digit(sym->st_value, 8);
 	else if (
 		(sym->info_type == STT_NOTYPE
@@ -52,17 +52,17 @@ void	ft_print_32(t_nm *nm, int index)
 	t_symbol_32	*sym;
 
 	sym = nm->bin_32.syms;
-	if (!sym)
-	{
-		print_prg_error(nm, INFO_NO_SYM);
-		return ;
-	}
 	if (index)
 	{
 		write(1, SEP_NL, 1);
 		write(1, nm->file_path, ft_strlen(nm->file_path));
 		write(1, SEP_1, 1);
 		write(1, SEP_NL, 1);
+	}
+	if (!sym)
+	{
+		print_prg_error(nm, INFO_NO_SYM);
+		return ;
 	}
 	while (sym)
 	{
